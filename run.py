6cca4872f54d00ee040cb41669c30782abe2b78b
Lee Guan Feng <lgf2111@Lees-MacBook-Pro.local>
2021-10-31 17:04:31 +0800

    changeed home to about

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html", active="home")
-
 @app.route("/request")
 def request():
     return render_template("request.html", active="request")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", active="about")
 
 @app.route("/tracking")
 def tracking():
