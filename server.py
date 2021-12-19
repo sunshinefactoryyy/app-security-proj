1849797a3df105834a31d0ebbcb5a14d9d096ba2
jon <jon.loh@hotmaili.com>
2021-12-19 20:59:20 +0800

    added color theme to social media icons and created login.html

--- server.py
+++ server.py
@@ -13,5 +13,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
