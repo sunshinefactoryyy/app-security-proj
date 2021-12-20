c74f78bdfb7b672fe48c89099fe2c820a754dc1b
Lee Guan Feng <lgf2111@Lees-MacBook-Pro-3.local>
2021-12-20 16:38:16 +0800

    format title

--- server.py
+++ server.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
