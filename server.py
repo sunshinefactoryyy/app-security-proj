6ad2adf2051b5b0a867ebc7c0436905a5133eac8
sunshinefactoryyy <88614714+sunshinefactoryyy@users.noreply.github.com>
2021-12-23 19:24:56 +0800

    personal info, requests and cart page

--- server.py
+++ server.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
+@app.route('/cusInfo')
+def cusInfo():
+    return render_template('cus_info.html')
+
+@app.route('/cusReq')
+def cusReq():
+    return render_template('cusReq.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
