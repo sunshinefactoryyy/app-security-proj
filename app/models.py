626f862a6c5b79befbeafd3d811857c166326c6e
jon <jon.loh@hotmaili.com>
2022-01-12 18:11:47 +0800

    Co-authored-by: lgf2111 <lgf2111@users.noreply.github.com>

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from sqlalchemy.orm import backref
 from app import db, login_manager
 from flask_login import UserMixin
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
