c92087a374408c5cf28ad9e67dc4d8a451bd3590
jon <jon.loh@hotmaili.com>
2022-01-12 18:52:24 +0800

    Co-authored-by: lgf2111 <lgf2111@users.noreply.github.com>
    tested inputs for Inventory class to add to site.database file

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import backref
 from app import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -12,25 +13,27 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    inventory_posts = db.relationship('Inventory', backref='user', lazy=True)
+    # inventory_posts = db.relationship('Inventory', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
 
-class Request(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    productName = db.Column(db.String(20), nullable=False)
-    productID = db.Column()
+# class Request(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     productName = db.Column(db.String(20), nullable=False)
+#     productID = db.Column()
 
 
 class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     part_name = db.Column(db.String, unique=True, nullable=False)
     part_quantity = db.Column(db.Integer, nullable=False)
+    part_cost = db.Column(db.Integer, unique=True, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
     user = db.Column(db.String, unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
 
     def __repr__(self):
-        return f"Inventory('{self.part_name}', '{self.part_quantity}', '{self.user}', '{self.author_id}', '{self.date_posted}')"
+        return f"Inventory('{self.part_name}', '{self.part_quantity}', '{self.user}', '{self.user_id}', '{self.date_posted}')"
+
