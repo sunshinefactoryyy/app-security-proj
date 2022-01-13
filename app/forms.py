14e11ed9da7f2113707dfea3e1994b64204214b0
Isaac Koh <isaackoh88@gmail.com>
2022-01-13 08:11:07 +0800

    Add. Employee account creation and update form.

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
-from app.models import Customer
+from app.models import Customer, Employee
 
 class LoginForm(FlaskForm):
     email = StringField("Email Address", validators=[DataRequired(), Email()])
@@ -43,5 +43,39 @@ class UpdateCustomerAccountForm(FlaskForm):
     def validate_email(self, email):
         if email.data != current_user.email:
             user = Customer.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("That email is taken. Please choose a different one.")
+
+class EmployeeCreationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email Address", validators=[DataRequired(), Email(message='Invalid email')])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+
+    def validate_username(self, username):
+        user = Employee.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is taken. Please choose a different one.")
+        
+    def validate_email(self, email):
+        user = Employee.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken. Please choose a different one.")
+
+class UpdateEmployeeCreationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email Address", validators=[DataRequired(), Email(message='Invalid email')])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = Employee.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username is taken. Please choose a different one.")
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = Employee.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError("That email is taken. Please choose a different one.")
