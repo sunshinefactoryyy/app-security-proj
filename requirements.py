f85f66c42c7725e64cfb7b06a58b3dc526f79aa6
lgf2111 <lgf2111@gmail.com>
2022-02-06 18:20:08 +0800

    implemented livereload and requirements.py

---
+++ requirements.py
@@ -0,0 +1,2 @@
+from os import system
+system("pip install -r requirements.txt")
--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from app import app
+from livereload import Server
 
 if __name__ == '__main__':
     # macOS/Linux
-    app.run(debug=True, ssl_context=('./ssl.crt', './ssl.key'))
+    # app.run(debug=True, ssl_context=('./ssl.crt', './ssl.key'))
     
     # Windows
     # app.run(debug=True, ssl_context=('.\\ssl.crt', '.\\ssl.key'))
+    
+    # Auto Reload Browser (Don't Login with Google)
+    app.debug = True
+    server = Server(app.wsgi_app)
+    server.serve()
