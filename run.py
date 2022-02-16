b35ecf16340f010d48a4fa8d5ce850c801e0601a
lgf2111 <lgf2111@gmail.com>
2022-02-16 23:54:23 +0800

    remove debug mode

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 from pathlib import Path
 
 if __name__ == '__main__':
-    app.run(debug=True,ssl_context=(Path('./ssl.crt'), Path('./ssl.key')))
+    app.run(ssl_context=(Path('./ssl.crt'), Path('./ssl.key')))
