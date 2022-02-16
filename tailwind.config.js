c6c151baff5c4aa48115592c64a814012c8855b3
lgf2111 <lgf2111@gmail.com>
2022-02-17 00:25:32 +0800

    added back essentials

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  prefix: 'tw-',
+  content: ["./app/templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
